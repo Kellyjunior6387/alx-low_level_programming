618e0edbb367bfb96c7fed384f4a4296c077071a
kellyjunior6387 <kellyjunior6386@gmail.com>
2023-09-09 00:48:34 +0300

    Task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always (0)
+ */
+int main(void)
+{
+int i;
+i = 0;
+while (i < 10)
+{
+	putchar(i);
+}
+	putchar('\n');
+	return (0);
+}
+
