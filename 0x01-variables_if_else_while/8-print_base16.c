d844b5cbb8ec011e73efa9bf388dc101d907b36c
kellyjunior6387 <kellyjunior6386@gmail.com>
2023-09-09 01:25:17 +0300

    Task 9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always (0)
+ */
+int main(void)
+{
+char number;
+char letter;
+number = '0';
+letter = 'a';
+while (number <= '9')
+{
+	putchar(number);
+	number++;
+}
+while (letter <= 'f')
+{
+	putchar(letter);
+	letter++;
+}
+	putchar('\n');
+	return (0);
+}
