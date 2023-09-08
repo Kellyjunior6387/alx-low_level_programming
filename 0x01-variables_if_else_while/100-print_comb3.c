3cb2ad8d7a1c2fa55d55905c351761d508edd5d1
kellyjunior6387 <kellyjunior6386@gmail.com>
2023-09-09 02:40:29 +0300

    Task 11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always (0)
+ */
+int main(void)
+{
+int first;
+int second;
+for (first = 0; first < 10; first++)
+{
+for (second = first + 1; second < 10; second++)
+{
+	putchar(first + '0');
+	putchar(second + '0');
+if (first != 8 || second != 9)
+{
+	putchar(',');
+	putchar('\n');
+}
+}
+}
+	return (0);
+}
