7190a336420fd470cfdb6769c4517a4285cd2a5b
kellyjunior6387 <kellyjunior6386@gmail.com>
2023-09-13 00:50:57 +0300

    J COLE

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,28 +10,22 @@ void print_to_98(int n)
 	int i;
 	int j;
 
-	if (n <= 98)
+	if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if (i != 98)
-			{
-				printf("%d, ", i);
-			}
-			else if (n >= 98)
-			{
-				for (j = n; j >= 98; j--)
-				{
-					if (j != 98)
-					{
-						printf("%d, ", j);
-					}
-					else if (j == 98)
-					{
-						printf("%d, ", j);
-					}
-				}
-			}
+			printf("%d, ", i);
 		}
+	}
+	else if (n > 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			printf("%d, ", j);
+		}
+	}
+	else if (n == 98)
+	{
+		printf("%d, ", n);
 	}
 }
