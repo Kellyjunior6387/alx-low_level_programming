60f6e34f021c4e6898f4c4994ef1ddc4e3f467af
kellyjunior6387 <kellyjunior6386@gmail.com>
2023-09-13 00:18:06 +0300

    Error

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,14 +1,11 @@
 #include "main.h"
-
 /**
  * print_times_table - print thr n times table starting with 0
  * @n: number of the table to be printed
  */
 void print_times_table(int n)
 {
-	int i;
-	int k;
-	int j;
+	int i, k, j;
 
 	if (n >= 0 && n <= 15)
 	{
@@ -29,7 +26,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(k + '0');
 				}
-				else if (k >= 10 && k <100)
+				else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -46,7 +43,4 @@ void print_times_table(int n)
 					_putchar((k % 10) + '0');
 				}
 			}
-			_putchar('\n');
-		}
-	}
-}
+			_putchar('\n'); }}}
