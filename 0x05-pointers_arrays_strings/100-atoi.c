ff59189ae594cf2e71f9dc1bdf56e4648c541b81
kellyjunior6387 <kellyjunior6386@gmail.com>
2023-09-18 23:47:08 +0300

    ERROR

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,32 +14,26 @@ int _atoi(char *s)
 	length = 0;
 	l = 0;
 	m = 0;
-
 	while (s[length] != '\0')
-	{
 		length++;
-	}
 	while (i < length && l == 0)
 	{
 		if (s[i] == '-')
-		{
 			++j;
-		}
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			m = s[i] - '0';
 			if (j % 2)
-			{
 				m = -m;
-			}
-			k = k * 10 + digit;
+			k = k * 10 + m;
 			l = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-			{
 				break;
-			}
 			l = 0;
 		}
 		i++;
 	}
+	if (l == 0)
+		return (0);
+	return (k);
 }
