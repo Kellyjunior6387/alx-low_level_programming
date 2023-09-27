f908ff2a1db1bf858db3a32f730af9be08c090ce
kellyjunior6387 <kellyjunior6386@gmail.com>
2023-09-27 17:10:57 +0300

    Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,6 +10,36 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	helper(n, 0, n);
-	return (0);
+	return (helper(n, 0, n));
+}
+/**
+ * helper function that uses binary search to find sqrt
+ * @n: input
+ * @i: start of search
+ * @j: end of search
+ * Return: sqrt
+ */
+int helper(int n, int i, int j)
+{
+        int mid;
+	int square;
+	
+	if (i > j)
+        {
+                return (-1);
+        }
+	mid = i + (j - i) / 2;
+        square = mid * mid;
+	if (n == square)
+        {
+                return (mid);
+        }
+        else if (square < n)
+        {
+                return (helper(n, mid + 1, j));
+        }
+        else
+        {
+                return (helper(n, i, mid + 1));
+        }
 }
