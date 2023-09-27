885da9e9edd0e6c507b9ed60da6d5a2ccbb46e81
kellyjunior6387 <kellyjunior6386@gmail.com>
2023-09-27 17:26:17 +0300

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,3 +9,29 @@ int is_prime_number(int n)
 	prime_number_helper(n, 2);
 	return (0);
 }
+/**
+ * _prime_number_helper - a function to check if a digit is a prime number
+ * @n: input
+ * @divisor: input
+ * Return: 1 if prime number
+ */
+int _prime_number_helper(int n, int divisor)
+{
+	if (n < 2)
+	{
+		return (0);
+	}
+	if (n == 2)
+	{
+		return (1);
+	}
+	if (n % divisor == 0)
+	{
+		return (0);
+	}
+	if (divisor * divisor <= n)
+	{
+		return (prime_number_helper(n, divisor + 1));
+	}
+	return (1);
+}
