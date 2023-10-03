62b1a6ebaa7db77ba119305e83605546d5ae2afd
Kellyjunior6387 <kellyjunior6386@gmail.com>
2023-10-03 01:24:02 -0700

    Malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,26 +9,23 @@
 char *_strdup(char *str)
 {
 	int i;
-	int length;
 	char *string;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (str[i] != '\0)
 	{
-		length++;
 		i++;
 	}
 	if (str == NULL)
 		return (NULL);
-	string = malloc(sizeof(char) * (length + 1));
+	string = malloc(sizeof(char) * (i + 1));
 
 	if (string == NULL)
 		return (NULL);
-	for (i = 0; i < length; i++)
+	for (i = 0; str[i]; i++)
 	{
 		string[i] = str[i];
 	}
-	string[length] = '\0';
 
 	return (string);
 }
