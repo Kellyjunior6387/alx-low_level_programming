edd2c423e367af37d056bdd75e17ffdb4fbc93b4
Kellyjunior6387 <kellyjunior6386@gmail.com>
2023-10-04 09:42:28 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -36,12 +36,12 @@ char *str_concat(char *s1, char *s2)
 	i = j = 0;
 	while (s1[i] != '\0')
 	{
-		string[i] = s1[i];
+		concat[i] = s1[i];
 		i++;
 	}
 	while (s2[j] != '\0')
 	{
-		string[i] = s2[j];
+		concat[i] = s2[j];
 		j++;
 		i++;
 	}
