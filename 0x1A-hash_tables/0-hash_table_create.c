2c21a8eaa68e5cb9f43c603d21a3eafd89313536
Kellyjunior6387 <kellyjunior6386@gmail.com>
2023-12-22 08:44:40 -0800

    hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,9 +13,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	new = malloc(sizeof(hash_table_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
-	
+	new->size = size;
 	new->array = malloc(sizeof(hash_node_t *) * size);
 	if (!new->array)
 	{
